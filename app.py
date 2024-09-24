b41908a2f205b06beb99553e2724a070789d94aa
ShirlyyJ <shirleyj13@nycstudents.net>
2024-09-24 10:13:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-values = [1,2.3,5,7,2,30,15]
-x = "test"
-for i in value:
-    print(f"hello {i}")
+Verb = []
+Noun = []
+Number = []
+Celebrity quest = []
+
+
+x = [1,2,3,4,5,6]
+print(f"number={x[1]}")
 
