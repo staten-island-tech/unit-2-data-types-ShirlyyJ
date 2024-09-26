e86494c306916b5b3bb7a23d6673ab09903643dd
ShirlyyJ <shirleyj13@nycstudents.net>
2024-09-26 10:10:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ count = sentence.split(' ')
 print(len(count)) """
 
 "python mad libs"
+
+"#2 challenge"
+number = int(input("input a number"))
+if number % 2:
+    print('even')
+else:
+    print('not even')
+
+"#5 challenge"
+""" a = 68
+b = 46
+for i in range(2, a+1): """
