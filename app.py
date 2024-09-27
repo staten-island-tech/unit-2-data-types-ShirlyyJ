b04181e9a5dcd02d9eef738af9ac2c3068170db6
ShirlyyJ <shirleyj13@nycstudents.net>
2024-09-27 10:19:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,21 @@ count = sentence.split(' ')
 print(len(count)) """
 
 "python mad libs"
+import random
+
+verb = ["ran", "jumped", "walked"]
+random_verb = random.choice(verb)
+print(random_verb)
 
 "#2 challenge"
-number = int(input("input a number"))
-if number % 2:
+""" number = int(input("input a number"))
+if number % 2 == 0:
     print('even')
 else:
-    print('not even')
+    print('not even') """
+
+"#3 challenge"
+
 
 "#5 challenge"
 """ a = 68
